7ee2eeffc397fdeac77992548195804c2137008b
Kassie Wang <kassie@dhcp-vl2041-616.eduroam.cornell.edu>
2021-05-10 18:38:04 -0400

    Fix create user script

--- create_user.py
+++ create_user.py
@@ -12,12 +12,12 @@ def main():
         db.metadata.create_all(db.engine)
         if User.query.all():
             print('A user already exists! Create another? (y/n):')
-            create = raw_input()
+            create = input()
             if create == 'n':
                 return
         
         print('Enter email address: ')
-        email = raw_input()
+        email = input()
         password = getpass()
         assert password == getpass('Password (again):')
 
